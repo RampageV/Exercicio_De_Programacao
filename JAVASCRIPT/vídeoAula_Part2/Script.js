38546ccf27aced7cf515b9f3886b21db63624968
Erick Pessoa Pinto Moreira <erick.pessoa.2014@hotmail.com>
2021-12-21 18:30:22 -0300

    Array,ToString,reverse e um pouco de objetos

---
+++ JAVASCRIPT/vídeoAula_Part2/Script.js
@@ -0,0 +1,13 @@
+
+var list = ['Uva', 'laranja', 'maça']
+
+// Ele reverte a ordem, se começa com uva, vai começar agora com maça.
+console.log(list.reverse());
+
+
+// Com toString, você passa de um array para uma lista de string, se você colocar list.toString()[0], ele vai imprimir a primeira letra da String que é U
+console.log(list.toString()[0]);
+
+//Verificação dos objetos
+var comida = [{Fruta:'maça', Cor:'Vermelho'},{Fruta:'Uva', Cor:'Roxo'}]
+console.log(comida[0].Fruta);
