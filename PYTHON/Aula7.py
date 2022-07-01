181a866449f0c0bec02ea1ff8e4e6e1bd16b2a71
Erick Pessoa <erick.pessoa.2014@hotmail.com>
2022-06-30 23:45:13 -0300

    PY array

---
+++ PYTHON/Aula7.py
@@ -0,0 +1,36 @@
+#--------------------Criando uma lista---------------------------------#
+
+lista = [1,2,5,7]
+lista_animal = ['cachorro','gato','elefante']
+
+#nova_lista =  lista_animal * 5 #Multiplica valores de um array
+#print(nova_lista)
+
+#print(lista_animal[1])
+
+#print(sum(lista)) #Soma do array
+#print(max(lista)) #Maior numero do array
+#print(min(lista)) # Menor numero
+
+#if 'gato' in lista_animal: # Verifica se tem o animal
+#    print('Existe esse animal na lista')
+#else:
+#   print('Não existe esse animal')
+#   lista_animal.append('lobo') #Append adiciona uma variavel no array
+#    print(lista_animal)  
+
+
+#lista_animal.pop() #Serve para tira ultima variavel do array
+#lista_animal.remove('cachorro') # Serve para remove sabendo o nome do elemento
+
+#lista.sort() #Sort serve para ordena listas.
+#print(lista)
+
+#lista_animal.sort() #Sort serve para ordena listas.
+#print(lista_animal)
+
+#lista_animal.reverse() #Ordena de trás para frente
+#print(lista_animal)
+
+#print(len(lista_animal)) # Informa a quantidade de elemento
+
