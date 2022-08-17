c1f49545122f784eb769c302751751674d434383
Erick Pessoa <erick.pessoa.2014@hotmail.com>
2022-08-16 21:51:11 -0300

    Atualização do projeto dia 16/08/2022

---
+++ JAVASCRIPT/TaskList/scripts.js
@@ -0,0 +1,19 @@
+const inputElement = document.querySelector('.new-task-input')/* O querySelector, retorna o primeiro elemento dentro do documento (usando ordenação em profundidade, pré-ordenada e transversal dos nós do documento) que corresponde ao grupo especificado de seletores.*/
+const addTaskButton = document.querySelector('.new-task-button')
+
+/* Função para remover os espaço em branco do input */
+const validateInput = () =>{
+      return inputElement.value.trim().lenght > 0; /* Returna se é true, o trim é o método que remove os espaço e verifica com o lenght se é maior que 0 */
+}
+
+const handleAddTask = () =>{
+
+         const InputIsValid = validateInput() // Verificando se o input é true
+
+        if(!InputIsValid){ // Se o input não for válido
+           return inputElement.classList.add("error") // A gente adicionar uma classe nele, para deixa ele vermelho. Precisa informa a tag que precisa coloca a nova classe e depois escrever classList, para acessa o método add, que vai adicionar uma nova classe e depois no css vai mudar os valores que desejar.
+        }         
+}
+
+addTaskButton.addEventListener('click',() => handleAddTask())
+
