435250875650deeb512f8585b3b8711a86735e2f
Erick Pessoa <erick.pessoa.2014@hotmail.com>
2022-09-30 23:10:20 -0300

    Organizando o código, transformando ele em Objetos

--- JAVASCRIPT/GUIZ/Javascript.js
+++ JAVASCRIPT/GUIZ/Javascript.js
@@ -4,28 +4,30 @@ let pergunta = {
     correto: 1
 }
 
-function start() {
-    
-    let alts = document.querySelectorAll('.alternativa')
-    alts.forEach(function (element, index) {
-        element.addEventListener('click', function () {
-            console.log('checa resposta')
-        })
-    });
+let app = {
 
-    mostraquestao(pergunta)
+    start: function () {
+        let alts = document.querySelectorAll('.alternativa')
+        alts.forEach(function (element, index) {
+            element.addEventListener('click', function () {
+                console.log('checa resposta')
+            })
+        });
+
+        mostraquestao(pergunta)
+    },
+
+    mostraquestao: function (q) {
+        // Mostra o titulo
+        let titleDiv = document.getElementById('titulo');
+        titleDiv.textContent = q.titulo; // Ele vai pegar o titulo da pergunta e substituir
+
+        // Mostra as alternativas
+        let alts = document.querySelectorAll('.alternativa');
+        alts.forEach(function (element, index) { // ForEach é igual ao um for ou map, dentro dele a gente passa um element e index dele, mas aqui no caso, estou passando uma função para cada elemento do ALTS
+            element.textContent = q.alternativas[index] // O element aqui está pegando o primeiro elemento do array e colocando com o textContent as alternativas
+        });
+    },
 }
 
-function mostraquestao(q) {
-    // Mostra o titulo
-    let titleDiv = document.getElementById('titulo');
-    titleDiv.textContent = q.titulo; // Ele vai pegar o titulo da pergunta e substituir
-
-    // Mostra as alternativas
-    let alts = document.querySelectorAll('.alternativa');
-    alts.forEach(function (element, index) { // ForEach é igual ao um for ou map, dentro dele a gente passa um element e index dele, mas aqui no caso, estou passando uma função para cada elemento do ALTS
-        element.textContent = q.alternativas[index] // O element aqui está pegando o primeiro elemento do array e colocando com o textContent as alternativas
-    });
-}
-
-start()
+app.start();
