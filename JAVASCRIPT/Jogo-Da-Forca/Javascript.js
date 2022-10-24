f34f39ddd181a947f634302940b28fe46629cb18
Erick Pessoa <erick.pessoa.2014@hotmail.com>
2022-10-24 17:06:37 -0300

    Atualizando lista do jogo da forca

---
+++ JAVASCRIPT/Jogo-Da-Forca/Javascript.js
@@ -0,0 +1,62 @@
+const palavras = [
+    palavra001 = {
+        nome: "IRLANDA",
+        categoria: "LUGARES",
+    },
+    palavra002 = {
+        nome: "EQUADOR",
+        categoria: "LUGARES",
+    },
+    palavra003 = {
+        nome: "BRASIL",
+        categoria: "LUGARES",
+    },
+    palavra004 = {
+        nome: "ÔNIBUS",
+        categoria: "TRANSPORTE",
+    },
+    palavra005 = {
+        nome: "LANCHA",
+        categoria: "TRANSPORTE",
+    },
+    palavra006 = {
+        nome: "BICICLETA",
+        categoria: "TRANSPORTE",
+    },
+    palavra007 = {
+        nome: "LÁPIS",
+        categoria: "OBJETOS",
+    },
+    palavra008 = {
+        nome: "CANETA",
+        categoria: "OBJETOS",
+    },
+    palavra009 = {
+        nome: "CADERNO",
+        categoria: "OBJETOS",
+    },
+    palavra010 = {
+        nome: "HAMBURGUER",
+        categoria: "ALIMENTOS",
+    },
+    palavra011 = {
+        nome: "COXINHA",
+        categoria: "ALIMENTOS",
+    },
+    palavra012 = {
+        nome: "PASTEL",
+        categoria: "ALIMENTOS",
+    },
+    palavra013 = {
+        nome: "GATO",
+        categoria: "ANIMAIS",
+    },
+    palavra014 = {
+        nome: "CACHORRO",
+        categoria: "ANIMAIS",
+    },
+    palavra015 = {
+        nome: "TATU",
+        categoria: "ANIMAIS",
+    },
+]
