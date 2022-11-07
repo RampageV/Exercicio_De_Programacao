4ebb53092702bbbf03b9f4d8258f3048e82af36c
Erick Pessoa <erick.pessoa.2014@hotmail.com>
2022-11-07 14:02:37 -0300

    Projeto slyde completo

---
+++ JAVASCRIPT/Slider_JAVASCRIPT/javascript.js
@@ -0,0 +1,13 @@
+/* Deixando o slider passando automaticamente !!*/
+
+let contador = 1
+
+setInterval(function () {
+
+    document.getElementById("slide" + contador).checked = true
+    contador++
+
+    if (contador > 4) {
+        contador = 1
+    }
+}, 3000)
