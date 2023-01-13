e32e107de036e27d6f3ab18635bd4fa45445cad4
Erick Pessoa <erick.pessoa.2014@hotmail.com>
2023-01-13 20:56:04 -0300

    Atualizando o menu

--- JAVASCRIPT/Portfolioreal/javascript.js
+++ JAVASCRIPT/Portfolioreal/javascript.js
@@ -1,4 +1,18 @@
-const palavrasubstituta = document.querySelector("ChamandoNome")
+const palavrasubstituta = document.querySelector(".ChamandoNome")
 const palavra = document.querySelectorAll(".palavra")
 
-console.log(palavra)
+const sobre = () => {
+    palavrasubstituta.innerHTML = palavra[0].innerHTML
+}
+
+const portfolio = () => {
+    palavrasubstituta.innerHTML = palavra[1].innerHTML
+}
+
+const contato = () => {
+    palavrasubstituta.innerHTML = palavra[2].innerHTML
+}
+
+const currículo = () => {
+    palavrasubstituta.innerHTML = palavra[3].innerHTML
+}
