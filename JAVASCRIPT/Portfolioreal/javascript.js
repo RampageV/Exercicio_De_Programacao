df65250ff44c8fd34de37454d0591404ea9a44b7
Erick Pessoa <erick.pessoa.2014@hotmail.com>
2023-01-30 21:44:52 -0300

    Atualizando curriculo no portfolio

--- JAVASCRIPT/Portfolioreal/javascript.js
+++ JAVASCRIPT/Portfolioreal/javascript.js
@@ -8,17 +8,18 @@ const sobre = () => {
     barraMenuAberto.style.display = "inline"; // Na classe css o display está como none, então colocando inline que é o padrão a barra vai aparacer
 }
 
-const portfolio = () => {
+const curriculo = () => {
     palavrasubstituta.innerHTML = palavra[1].innerHTML
     barraMenuAberto.style.display = "inline";
 }
 
-const contato = () => {
+const portfolio = () => {
     palavrasubstituta.innerHTML = palavra[2].innerHTML
     barraMenuAberto.style.display = "inline";
 }
 
-const currículo = () => {
+const contato = () => {
     palavrasubstituta.innerHTML = palavra[3].innerHTML
     barraMenuAberto.style.display = "inline";
 }
+
