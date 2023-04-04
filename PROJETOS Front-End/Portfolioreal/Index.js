43d5fc53d5fb311a0de848fd29b8b377957b3886
erick-pessoa <erick.pessoa.2014@hotmail.com>
2023-04-03 23:57:25 -0300

    Portifolio atualizado 100%

--- PROJETOS Front-End/Portfolioreal/Index.js
+++ PROJETOS Front-End/Portfolioreal/Index.js
@@ -3,5 +3,5 @@ const enviarMensagem = document.getElementById("buttonEnviar")
 enviarMensagem.addEventListener('click', () => {
     const nome = document.getElementById("nome").value;
 
-    alert("Obrigado(a): " + nome +", "+"pela sua mensagem, entraremos em contato o mais breve possível.");
+    alert("Obrigado(a): " + nome +" pela sua mensagem, entraremos em contato o mais breve possível.");
 });
