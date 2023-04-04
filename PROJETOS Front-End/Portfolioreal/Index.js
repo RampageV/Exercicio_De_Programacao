e8b6acb4d3d62170f9fdce3ed17a2dc33c5f1278
erick-pessoa <erick.pessoa.2014@hotmail.com>
2023-04-03 23:49:54 -0300

    Correção na parte do javascript em relação ao alert()

--- PROJETOS Front-End/Portfolioreal/Index.js
+++ PROJETOS Front-End/Portfolioreal/Index.js
@@ -1,5 +1,7 @@
 const enviarMensagem = document.getElementById("buttonEnviar")
 
-enviarMensagem.addEventListener('click',()=>{
-alert("Obrigado, mesangem enviada com sucesso !!")
+enviarMensagem.addEventListener('click', () => {
+    const nome = document.getElementById("nome").value;
+
+    alert("Obrigado(a): " + nome +", "+"pela sua mensagem, entraremos em contato o mais breve possível.");
 });
