b093a213507f6773bc19e0fda2eab9728cf52c05
erick-pessoa <erick.pessoa.2014@hotmail.com>
2023-04-09 20:39:15 -0300

    Colocando o botão delete dentro de cada linha

--- PROJETOS Front-End/TabelaDeEnderecoIP/Index.js
+++ PROJETOS Front-End/TabelaDeEnderecoIP/Index.js
@@ -1,19 +1,52 @@
-function AdicionarInformacao(ip, mask, versao) {
+function AdicionarInformacao() {
 
     var tabela = document.getElementById("informacao");
-    var qtsLinhas = tabela.rows.length;
-    var linha = tabela.insertRow(qtsLinhas);
+    var ipp = document.getElementById("TextInput").value;
+    var maskk = document.getElementById("TextMask").value;
+    var versaoo = document.getElementById("TextVersao").value;
+
+    var qtsLinhas = tabela.rows.length; // Contagem de linhas.
+    var linha = tabela.insertRow(qtsLinhas); // Inserindo uma linha abaixo da quantidade total de linha. 
 
 
-    var colunaIp = linha.insertCell(0);
-    var colunaMask = linha.insertCell(1);
-    var colunaVersao = linha.insertCell(2);
+    var colunaIp = linha.insertCell(0); // Coluna do IP
+    var colunaMask = linha.insertCell(1); // Coluna da Mask
+    var colunaVersao = linha.insertCell(2); // Coluna da Versao
+    var colunaExcluir = linha.insertCell(3); // Coluna do excluir
 
-    colunaIp.innerHTML = ip;
-    colunaMask.innerHTML = mask;
-    colunaVersao.innerHTML = versao;
+    // Criando o botão excluir
+    var botaoExcluir = document.createElement("span");
+    botaoExcluir.classList.add("btnExcluir"); // Colocando a class btnExcluir dentro do botão span
+    botaoExcluir.textContent = "Deleta"; // Colocando o nome do botão que vai aparecer para o usuário.
+
+    // Exercução do botão excluir dentro da tabela
+    tabela.addEventListener("click", function (event) {
+        var clickElemento = event.target; // Pegando a target, quando for clicado
+
+        if (clickElemento.classList.contains("btnExcluir")) { // Verificando se tem a classe btnExcluir.
+            var celular = clickElemento.parentNode; // Pegando o elemento filho para conseguir acessar o elemento pai
+            var linha = celular.parentNode; // Pegando elemento pai para excluir os elementos filhos que estão dentro dele. 
+            linha.remove() // removendo
+        }
+    });
+
+    colunaIp.innerHTML = ipp;
+    colunaMask.innerHTML = maskk;
+    colunaVersao.innerHTML = versaoo;
+    colunaExcluir.appendChild(botaoExcluir); //Colocando o conteudo dentro da linha criado.
 
     document.getElementById("TextInput").value = null;
     document.getElementById("TextMask").value = null;
     document.getElementById("TextVersao").value = null;
+
+}
+
+
+
+function remove() {
+
+    var remove = document.createElement("p")
+    remove.textContent = "x"
+
+    console.log(remove)
 }
