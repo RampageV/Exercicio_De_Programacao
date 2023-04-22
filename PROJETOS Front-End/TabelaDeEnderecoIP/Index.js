e4d1b42e1904b3ddbe738a3e6ae4324e3e93e819
erick-pessoa <erick.pessoa.2014@hotmail.com>
2023-04-22 17:10:26 -0300

    Colocando os icones de salve e dele nos botões

--- PROJETOS Front-End/TabelaDeEnderecoIP/Index.js
+++ PROJETOS Front-End/TabelaDeEnderecoIP/Index.js
@@ -9,13 +9,11 @@ function AdicionarInformacao() {
     var qtsLinhas = tabela.rows.length; // Contagem de linhas. 11
     var linha = tabela.insertRow(qtsLinhas); // Inserindo uma linha abaixo da quantidade total de linha. 
 
-    linha.setAttribute("class", "New-row" + qtsLinhas);
-
-
     var colunaIp = linha.insertCell(0); // Coluna do IP
     var colunaMask = linha.insertCell(1); // Coluna da Mask
     var colunaVersao = linha.insertCell(2); // Coluna da Versao
-    var colunaExcluir = linha.insertCell(3); // Coluna do excluir
+    var colunaEditarESalve = linha.insertCell(3)
+    var colunaExcluir = linha.insertCell(4); // Coluna do excluir
 
 
     colunaIp.classList.add("New-cell" + qtsLinhas);
@@ -28,8 +26,6 @@ function AdicionarInformacao() {
     salvar()
     EspaçoVazio()
     excluir()
-
-
 
     /*  -----------------------------------------------------------Funções------------------------------------------ */
     function EspaçoVazio() {
@@ -48,43 +44,40 @@ function AdicionarInformacao() {
 
     function editar() {
 
-
         var editarbutton = document.createElement("span" + qtsLinhas);
+        editarbutton.setAttribute("class", "fa-solid fa-pen")
         editarbutton.classList.add("btnEditar" + qtsLinhas);
-        editarbutton.setAttribute("id", "btnEditar")
-        editarbutton.textContent = "Editar"
-        colunaExcluir.appendChild(editarbutton)
+        colunaEditarESalve.appendChild(editarbutton)
 
         tabela.addEventListener("click", function (event) {
             var clickElemento = event.target
             if (clickElemento.classList.contains("btnEditar" + qtsLinhas)) {
 
                 var ipLinha = document.querySelector(".New-cell" + qtsLinhas)
-                ipLinha.innerHTML = "<input type='number' id='ip_text' value=''> </td>";
+                var ValorLinha = ipLinha.innerHTML;
+                ipLinha.innerHTML = "<input type='number' id='ip_text" + qtsLinhas + "' value='" + ValorLinha + "'> </td>";
 
+                console.log(ipLinha)
                 document.querySelector(".btnEditarSalve" + qtsLinhas).style.display = "inline"
                 document.querySelector(".btnEditar" + qtsLinhas).style.display = "none"
             }
-
         });
-        
     }
 
     function salvar() {
-
 
         var editar_salve = document.createElement("span" + qtsLinhas)
         editar_salve.classList.add("btnEditarSalve" + qtsLinhas);
         editar_salve.setAttribute("id", "editar_salve")
         editar_salve.textContent = "Salvar"
-        colunaExcluir.appendChild(editar_salve)
+        colunaEditarESalve.appendChild(editar_salve)
 
         tabela.addEventListener("click", function (event) {
 
             var clickElemento = event.target
             if (clickElemento.classList.contains("btnEditarSalve" + qtsLinhas)) {
 
-                var valorDoSalve = document.getElementById("ip_text").value;
+                var valorDoSalve = document.getElementById("ip_text"+ qtsLinhas).value;
                 document.querySelector(".New-cell" + qtsLinhas).innerHTML = valorDoSalve
 
                 document.querySelector(".btnEditarSalve" + qtsLinhas).style.display = "none"
@@ -97,25 +90,25 @@ function AdicionarInformacao() {
 
     function excluir() {
 
-        var botaoExcluir = document.createElement("span" + qtsLinhas);
-        botaoExcluir.classList.add("btnExcluir" + qtsLinhas); // Colocando a class btnExcluir dentro do botão span
-        botaoExcluir.setAttribute("id", "btnExcluir")
-        botaoExcluir.textContent = "Deletar"; // Colocando o nome do botão que vai aparecer para o usuário.
+        var botaoExcluir = document.createElement("span");
+        botaoExcluir.setAttribute("class", "fa-solid fa-x")
+        botaoExcluir.classList.add("btnExcluir"); // Colocando a class btnExcluir dentro do botão span
         colunaExcluir.appendChild(botaoExcluir); //Colocando o conteudo dentro da linha criado.
 
-        // Exercução do botão excluir dentro da tabela
         tabela.addEventListener("click", function (event) {
 
             var clickElemento = event.target; // Pegando a target, quando for clicad
-            if (clickElemento.classList.contains("btnExcluir" + qtsLinhas)) { // Verificando se tem a classe btnExcluir.
+            if (clickElemento.classList.contains("btnExcluir")) { // Verificando se tem a classe btnExcluir.
                 var celular = clickElemento.parentNode; // Pegando o elemento filho para conseguir acessar o elemento pai
                 var linha = celular.parentNode; // Pegando elemento pai para excluir os elementos filhos que estão dentro dele. 
                 linha.remove()
 
-
-
-
+                console
             }
         });
     }
+
+    document.getElementById("TextInput").value = " ";
+    document.getElementById("TextMask").value = " ";
+    document.getElementById("TextVersao").value = " ";
 }
