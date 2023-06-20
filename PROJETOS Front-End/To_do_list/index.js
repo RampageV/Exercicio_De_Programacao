9d15cb3c8c3c07e035da6d8f15e92f021fa649f5
Erick Pessoa <erick.pessoa.2014@hotmail.com>
2023-06-20 00:23:00 -0300

    Criado a classe deleteAndCheck que serve para deletar e checka os botões de click

--- PROJETOS Front-End/To_do_list/index.js
+++ PROJETOS Front-End/To_do_list/index.js
@@ -6,30 +6,50 @@ const todoList = document.querySelector('.todo-list')
 // Funções
 const addTodo = (event) => {
     event.preventDefault(); // Serve para não atualizar a página por esse está em um FORM de formulário.
-    
+
     const todoDiv = document.createElement('div');
     todoDiv.classList.add('todo')
 
     const todoLi = document.createElement('li')
     todoLi.classList.add('todo-list')
-    todoLi.innerHTML =  todoInput.value;
+    todoLi.innerHTML = todoInput.value;
+    todoInput.value = "";
+
     todoDiv.appendChild(todoLi) // Adicionando li dentro da div
 
     const completedButtom = document.createElement('button')
     completedButtom.classList.add('completed-btn')
     completedButtom.innerHTML = '<i class="fa-solid fa-square-check"></i>'
+
     todoDiv.appendChild(completedButtom) // Adicionando completed button dentro da div
 
     const trashButtom = document.createElement('button')
     trashButtom.innerHTML = '<i class="fa-solid fa-xmark"></i>';
     trashButtom.classList.add('trash-btn')
+
     todoDiv.appendChild(trashButtom) // Adicionando button delete dentro da div
 
     todoList.appendChild(todoDiv) // Adicinonando a tag Div dentro da lista de ul.
 
-    todoInput.value = "";
 }
 
+const deleteAndCheck = (e) => {
+
+    const item = e.target;
+
+    if (item.classList[0] === 'trash-btn') {
+
+        const todo = item.parentElement;
+        todo.remove();
+
+    } else if (item.classList[0] == 'completed-btn') {
+
+        const todo = item.parentElement;
+        todo.classList.toggle('completed')
+    }
+
+}
 
 // Eventos de excutas
 todoButton.addEventListener('click', addTodo)
+todoList.addEventListener('click', deleteAndCheck)
