a75823bb09c5b454359fdf8ac8530e1102267197
Erick Pessoa <erick.pessoa.2014@hotmail.com>
2023-06-29 16:16:51 -0300

    Concluindo botões de completo e não completo

--- PROJETOS Front-End/To_do_list/index.js
+++ PROJETOS Front-End/To_do_list/index.js
@@ -2,6 +2,9 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button')
 const todoList = document.querySelector('.todo-list')
+const filterOption = document.querySelector('.filter-todo')
+
+
 
 // Funções
 const addTodo = (event) => {
@@ -12,25 +15,26 @@ const addTodo = (event) => {
 
     const todoLi = document.createElement('li')
     todoLi.classList.add('todo-list')
-    todoLi.innerHTML = todoInput.value;
-    todoInput.value = "";
+    todoLi.innerText = todoInput.value;
 
     todoDiv.appendChild(todoLi) // Adicionando li dentro da div
 
-    const completedButtom = document.createElement('button')
-    completedButtom.classList.add('completed-btn')
-    completedButtom.innerHTML = '<i class="fa-solid fa-square-check"></i>'
 
+    const completedButtom = document.createElement('button')
+    completedButtom.innerHTML = '<i class="fa-solid fa-square-check"></i>'
+    completedButtom.classList.add('complete-btn');
     todoDiv.appendChild(completedButtom) // Adicionando completed button dentro da div
 
-    const trashButtom = document.createElement('button')
-    trashButtom.classList.add('trash-btn')
-    trashButtom.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    saveLocalTodos(todoInput.value)
 
+    const trashButtom = document.createElement('button')
+    trashButtom.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    trashButtom.classList.add('trash-btn')
     todoDiv.appendChild(trashButtom) // Adicionando button delete dentro da div
 
     todoList.appendChild(todoDiv) // Adicinonando a tag Div dentro da lista de ul.
 
+    todoInput.value = "";
 }
 
 const deleteAndCheck = (e) => {
@@ -43,13 +47,58 @@ const deleteAndCheck = (e) => {
         todo.addEventListener('transitioned', () => { // Efeito de transição quando algo é deletado
             todo.remove();
         });
+    }
 
-    } else if (item.classList[0] == 'completed-btn') { // Completado
+    if (item.classList[0] === 'complete-btn') { // Completado
         todo.classList.toggle('completed');
     }
 
 }
 
+const filterTodo = (e) => {
+    const todos = todoList.childNodes;
+
+    todos.forEach((todo) => {
+
+        switch (e.target.value) {
+            case 'all':
+                todo.style.display = 'flex'
+                break;
+            case "completed":
+                if (todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+                break;
+            case "uncompleted":
+                if (!todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+                break;
+        }
+    })
+}
+
+function saveLocalTodos(Todo) {
+
+    let todos;
+
+    // Verifica se o localstorage está vazio, se tiver ele cria um array
+    if (localStorage.getItem('todos') == null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos')) // Se caso não tiver vazio, ele vai converter os tudo que está na lista para um formato json.
+    }
+
+    todos.push(Todo); // Estou empurrando todas as informações que vem do meu usuario, a minha variavel "todos"
+
+    localStorage.setItem('todoss', JSON.stringify(todos)) // Aqui a gente seta as informações no banco de dados em formato de JSON no local storange. 
+}
+
 // Eventos de excutas
 todoButton.addEventListener('click', addTodo)
 todoList.addEventListener('click', deleteAndCheck)
+filterOption.addEventListener('click', filterTodo)
