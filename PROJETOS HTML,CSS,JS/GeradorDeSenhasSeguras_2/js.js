e4db2cfbeae646a195db1a3826bf1619591962fb
Erick Pessoa <erick.pessoa.2014@hotmail.com>
2023-08-25 17:35:58 -0300

    Projeto terminado

--- PROJETOS HTML,CSS,JS/GeradorDeSenhasSeguras_2/js.js
+++ PROJETOS HTML,CSS,JS/GeradorDeSenhasSeguras_2/js.js
@@ -7,7 +7,7 @@ let password = document.querySelector("#password")
 let containerPassword = document.querySelector("#container-password")
 
 let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!";
-let NovaSenha = "";
+let NovaSenha = '';
 
 
 SizePassword.innerHTML = sliderElement.value;
@@ -17,5 +17,25 @@ slider.oninput = function () {
 }
 
 const generatePassword = () => {
-alert("Tudo ok")
+
+    let pass = ""; // Aqui é onde vai ficar a senha
+
+    for (let i = 0, numTotalCharset = charset.length; i < sliderElement.value; ++i) {
+
+        // CharAt retorna o caractere em uma localização específica em uma String;
+        // Math.floor retorna um numero inteiro
+        // Math.random retorna um número randomico vezes o numero total do charset.  
+        pass += charset.charAt(Math.floor(Math.random() * numTotalCharset));
+    }
+
+    containerPassword.classList.remove('hide');
+    password.innerHTML = pass;
+    NovaSenha = pass;
 }
+
+function copyPassword() {
+    alert("Senha copiada com sucesso!")
+    console.log(NovaSenha)
+    navigator.clipboard.writeText(NovaSenha);
+}
+
