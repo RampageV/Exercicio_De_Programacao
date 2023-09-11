3948591fbb610a042b2e72e7c52172a6d798fd73
Erick Pessoa <erick.pessoa.2014@hotmail.com>
2023-09-11 17:26:26 -0300

    Montando a tabela e o localStorange

---
+++ PROJETOS HTML,CSS,JS/CRUD BÁSICO/script.js
@@ -0,0 +1,33 @@
+var dados = []
+
+
+const PopulaTabela = () => {
+
+    if (Array.isArray(dados)) {
+
+        $("#tblDados tbody").html("") // Limpando as linhas da minha tabela para escrever novas 
+
+        dados.forEach(function (item) {
+            //Template String
+            $("#tblDados tbody").append(`<tr>
+            <td>${item.ID}</td>
+            <td>${item.Nome}</td>
+            <td>${item.Sobrenome}</td>
+            <td>${item.DtNascimento}</td>
+            <td>${item.Formação}</td>
+            </tr>`);
+        });
+    }
+}
+
+
+$(function () { // jQuery
+
+    //Exercuta ao carregar a tela.
+    dados = JSON.parse(localStorage.getItem("__dados__")) // Convertendo para objeto.
+
+    if (dados) { // Se diferente de vazio, vai me retornar True.
+        PopulaTabela()
+    }
+
+});
