4f9434a4dc8eb65abd951fb28e973483ff96754b
Erick Pessoa <erick.pessoa.2014@hotmail.com>
2023-09-29 22:59:14 -0300

    Projeto finalizado, só precisa de um ajuste na hora da edição

--- PROJETOS HTML,CSS,JS/CRUD BÁSICO/script.js
+++ PROJETOS HTML,CSS,JS/CRUD BÁSICO/script.js
@@ -28,6 +28,7 @@ const EditaRegistro = (id) => {
     /*Mostra os valores de todos os campos.*/
     dados.forEach(function (item) {
         if (item.ID == id) {
+            $("hdID").val(item.ID)
             $("#txtNome").val(item.Nome)
             $("#txtSobrenome").val(item.Sobrenome)
             $("#txtDtNascimento").val(item.DtNascimento.substr(6, 4) + "-" + item.DtNascimento.substr(3, 2) + "-" + item.DtNascimento.substr(0, 2)) // O componente só compreende o formato americano da data. E atravez do substr, a gente vai troca as posições para o componente conseguir ler o formato brasileiro.
@@ -39,12 +40,12 @@ const EditaRegistro = (id) => {
 function PopulaTabela() {
 
     if (Array.isArray(dados)) {
+
         localStorage.setItem("__dados__", JSON.stringify(dados)) // Depois que verifica se é um array mesmo, ele salva no localStorage.
 
         $("#tblDados tbody").html("") // Limpando as linhas da minha tabela para escrever novas 
 
         dados.forEach(function (item) {
-
             //Template String
             $("#tblDados tbody").append(`<tr>
                 <td>${item.ID}</td>
@@ -70,6 +71,7 @@ $(function () {
 
     $("#btnSalvar").click(function () { // event de click
 
+        let _id = $("#hdID").val()
         let Nome = $("#txtNome").val()
         let Sobrenome = $("#txtSobrenome").val()
         let DtNascimento = new Date($("#txtDtNascimento").val()).toLocaleDateString("pt-br", {
@@ -77,20 +79,34 @@ $(function () {
         }) // Passando o valor do input para o new date, pegando o valor é convertendo para o formato brasileiro: 03/05/2016, e usando timeZone: UTC para cancelar todo fuso horário.
         let Formacao = $("#txtFormacao").val()
 
-        var registro = {}
 
-        registro.Nome = Nome;
-        registro.Sobrenome = Sobrenome;
-        registro.DtNascimento = DtNascimento;
-        registro.Formacao = Formacao;
+        if (!_id || _id == "0") { // Se for nula, vazia ou igual a zero, vai da true. Significa que eu não tenho um id válido, então eu vou ter que cadastra um novo registro
+            
+            let registro = {}
 
-        registro.ID = dados.length + 1;
+            registro.Nome = Nome;
+            registro.Sobrenome = Sobrenome;
+            registro.DtNascimento = DtNascimento;
+            registro.Formacao = Formacao;
 
-        dados.push(registro)
+            registro.ID = dados.length + 1;
+            dados.push(registro)
+
+        } else {
+            dados.forEach(function (item) { // Se eu tiver esse ID válido, significa que eu não tenho que incluir um novo registro e tenho que realiza a edição do registro que eu tenho no id amazenado.   
+                if (item.ID == _id) {
+                    item.Nome = Nome
+                    item.Sobrenome = Sobrenome
+                    item.DtNascimento = DtNascimento
+                    item.Formacao = Formacao
+                }
+            });
+        }
 
         alert("Registro Salvo com Sucesso !!")
-
+        
         /* Apagando o campo !! */
+        $("#hdID").val("0") // Sem ele, toda vez que for editar mais de um registro, vai ficar com o valor do registro anterior.
         $("#txtNome").val("")
         $("#txtSobrenome").val("")
         $("#txtDtNascimento").val("")
