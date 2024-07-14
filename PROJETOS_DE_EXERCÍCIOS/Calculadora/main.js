f861ae3312ee7270ef2253b6187eed838638f876
Erick Pessoa <erick.pessoa.2014@hotmail.com>
2024-07-14 20:40:18 -0300

    ok

--- PROJETOS_DE_EXERCÍCIOS/Calculadora/main.js
+++ PROJETOS_DE_EXERCÍCIOS/Calculadora/main.js
@@ -1,30 +1,36 @@
-const previousOperationText = document.querySelector("#revious-operation") 
+const previousOperationText = document.querySelector("#revious-operation")
 const currentOperationText = document.querySelector("#current-operation")
-const buttons = document.querySelectorAll("#buttons-container button"); /*Selecionando todo os botoes*/
+const buttons = document.querySelectorAll(
+	"#buttons-container button"
+) /*Selecionando todo os botoes*/
 
+class Calculator {
+	constructor(previousOperationText, currentOperationText) {
+		this.previousOperationText = previousOperationText
+		this.currentOperationText = currentOperationText
+		this.currentOperation = ""
+	}
 
-class Calculator{
-    constructor(previousOperationText,currentOperationText){
-      this.previousOperationText = previousOperationText;
-      this.currentOperationText = currentOperationText;
-      this.currentOperation = "";
- }
+	addDigit(digit) {
+		this.currentOperation = digit
+		this.updateScreen()
+	}
+
+	updateScreen() {
+		this.currentOperationText.innerText += this.currentOperation
+	}
 }
 
-addDigit(digit){
-    console.log(digit)
-}
+const cal = new Calculator(previousOperationText, currentOperationText)
 
+buttons.forEach((btn) => {
+	btn.addEventListener("click", (e) => {
+		const value =
+			e.target
+				.innerText /*Quando é botão a gente pegar inneText, se for input pega value*/
 
-const cal = new Calculator(previousOperationText,currentOperationText)
-
-
-buttons.forEach((btn)=>{
-    btn.addEventListener("click",(e)=>{
- const value = e.target.innerText; /*Quando é botão a gente pegar inneText, se for input pega value*/
-
-if(+value >= 0 || value === "."){
-                                                         console.log(typeof value)
-}
-    });
+		if (+value >= 0 || value === ".") {
+			cal.addDigit(value)
+		}
+	})
 })
